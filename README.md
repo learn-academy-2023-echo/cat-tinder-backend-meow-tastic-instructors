057b40540eef4dd2f3c53f4b2bd90cceda075326
Elyse Montano <elysemontano.dev.com>
2023-09-05 15:06:18 -0700

    adds api endpoits and request specs

--- README.md
+++ README.md
@@ -1,24 +1,13 @@
-# README
+# API Endpoints
+- To see a list of databases, run: `psql`
+  - type \l
+  - to exit `q`
+    `exit`
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Point of communication 
+- Get a request from your route which will call on your controller method
+- Send json as data since we are working on an API
 
-Things you may want to cover:
+Practice TDD - write tests, fail, write code, pass
+Tests are written in spec/requests/cats_spec.rb for our request specs
 
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -2,6 +2,98 @@ require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of cats" do 
+      Cat.create(
+        {
+          name: 'Felix',
+          age: 2,
+          enjoys: 'Long naps on the couch, and a warm fire.',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      )
+      # Make a request to endpoint
+      get '/cats'
+      # Specify data we want back in format of JSON
+      cat = JSON.parse(response.body)
+      # Assert to check that the response is a successful response of 200
+      expect(response).to have_http_status(200)
+      expect(cat.length).to eq 1
+    end
+  end
+
+  describe "POST /create" do 
+    it "creates a cat" do 
+      cat_params = {
+        cat: {
+          name: 'Felix',
+          age: 2,
+          enjoys: 'Long naps on the couch, and a warm fire.',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      # Send the request to the server and pass params which are cat_params
+      post '/cats', params: cat_params
+      # Assert that this request was successful
+      expect(response).to have_http_status(200)
+
+      cat = Cat.first 
+
+      p cat.id
+      expect(cat.name).to eq 'Felix'
+      expect(cat.age).to eq 2
+      expect(cat.enjoys).to eq 'Long naps on the couch, and a warm fire.'
+    end
+  end
+
+  describe "PATCH /update" do 
+    it "updates a cat" do 
+      # First create a cat so we have an instance to update
+      cat_params = {
+        cat: {
+          name: 'Felix',
+          age: 2,
+          enjoys: 'Long naps on the couch, and a warm fire.',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      # Send the request to the server and pass params which are cat_params
+      post '/cats', params: cat_params
+      cat = Cat.first 
+
+      updated_cat_params = {
+        cat: {
+          name: 'Felix',
+          age: 3,
+          enjoys: 'Long naps on the couch, and a warm fire.',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      # Make a request to update cat using string interpolation for the cat id
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      # We need to grab the instance of cat after it has been updated to assert against it.
+      updated_cat = Cat.find(cat.id)
+      expect(response).to have_http_status(200)
+      expect(updated_cat.age).to eq 3 
+    end
+  end
+
+  describe "DELETE /destroy" do 
+    it "deletes a cat" do 
+      cat_params = {
+        cat: {
+          name: 'Felix',
+          age: 2,
+          enjoys: 'Long naps on the couch, and a warm fire.',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+
+      post "/cats", params: cat_params
+      cat = Cat.first
+      delete "/cats/#{cat.id}"
+      expect(response).to have_http_status(200)
+      cats = Cat.all 
+      expect(cats).to be_empty
+    end
   end
 end
